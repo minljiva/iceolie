bdc23c7ba919e7847a4d9ec17768a3943ced651a
solie <hall2gare@gmail.com>
2026-04-26 18:09:42 +0200

    Update projets.js

--- scripts/projets.js
+++ scripts/projets.js
@@ -1,4 +1,4 @@
- fconst SUPABASE_URL = "https://ljkaqpgpfczqvpobwzoo.supabase.co";
+const SUPABASE_URL = "https://ljkaqpgpfczqvpobwzoo.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxqa2FxcGdwZmN6cXZwb2J3em9vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5Mjg4ODYsImV4cCI6MjA5MjUwNDg4Nn0.N813WeFmHTGJvgAcSgRMTSYmog21fJn979OU__urZjY";
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
